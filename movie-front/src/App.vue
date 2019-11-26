683b316bd9edd8da7261e50b8d71586aae2cd0df
JHKim <didwjd169@gmail.com>
2019-11-26 19:12:22 +0900

    add review_list & profile base

--- movie-front/src/App.vue
+++ movie-front/src/App.vue
@@ -14,7 +14,7 @@
             <v-list-item-title>Movie List</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link>
+        <v-list-item link to="/login">
           <v-list-item-action>
             <v-icon>mdi-alpha-w-box</v-icon>
           </v-list-item-action>
@@ -22,7 +22,7 @@
             <v-list-item-title>World Cup</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link to="/login">
+        <v-list-item link to="/profile">
           <v-list-item-action>
             <v-icon>mdi-account</v-icon>
           </v-list-item-action>
--- movie-front/src/router/index.js
+++ movie-front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
   },
 ]
 

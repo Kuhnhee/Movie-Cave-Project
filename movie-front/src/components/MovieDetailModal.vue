e052a29d6e5298cb5503d6ec2e1e6653d6cc44bd
이건희 <sheva0902@naver.com>
2019-11-27 19:40:14 +0900

    Home: movielist, card, modal

--- movie-front/src/components/MovieDetailModal.vue
+++ movie-front/src/components/MovieDetailModal.vue
@@ -23,8 +23,9 @@
       <v-divider></v-divider>
 
       <v-card-text>
-        <div><b>평점</b></div>
-        <div>{{ movie.rate }}</div>
+        <div><b>감독</b> | <span v-for="name in directors" :key=name>{{ name }}, </span></div>
+        <div><b>배우</b> | <span v-for="name in actors" :key=name>{{ name }}, </span></div>
+        <div><b>평점</b> | <span>{{ movie.rate }}</span></div>
       </v-card-text>
 
       <v-divider></v-divider>
@@ -40,10 +41,18 @@
       
       <v-divider></v-divider>
 
+      <v-row>
+        <v-col cols="9">
+          <v-text-field label="댓글" v-model="new_comment"></v-text-field>
+        </v-col>
+        <v-col cols="3" class="mt-3">
+          <v-btn @click="writeComment">
+            <v-icon>mdi-pencil</v-icon>입력하기
+          </v-btn>
+        </v-col>
+
+      </v-row>
       
-
-
-
 
     </v-container>
 
@@ -55,24 +64,91 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      // dialog: false,
+      directors: [],
+      actors: [],
+      new_comment: '',
     }
   },
+
   props: {
     movie: {
       type: Object,
       required: false,
     }
   },
+
   methods: {
     closeDialog() {
       this.$emit('closeDialogEvent', true)
-    }
-  }
+    },
 
+    directorsNameCall() {
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      this.movie.directors.forEach(code => {
+        axios.get(`http://localhost:8000/api/v1/director/${code}/`, options)
+        .then(res => {
+          this.directors.push(res.data.name)
+        })
+        .catch(error => {
+          console.log(error.response)
+        })
+      })
+    },
+
+    actorsNameCall() {
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      this.movie.actors.forEach(code => {
+        axios.get(`http://localhost:8000/api/v1/actor/${code}/`, options)
+        .then(res => {
+          this.actors.push(res.data.name)
+        })
+        .catch(error => {
+          console.log(error.response)
+        })
+      })
+    },
+
+    writeComment() {
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        },
+        body: {
+          content: this.new_comment 
+        }
+      }
+
+      axios.get(`http://localhost:8000/api/v1/review/comment/`, options)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+    }
+
+  }, //end of methods
+
+  mounted() {
+    this.directorsNameCall()
+    this.actorsNameCall()
+  }
 }
 </script>
 

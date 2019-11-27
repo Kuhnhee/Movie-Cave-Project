5bfe1feac6037e0df4c3e7f91c38b0e75ce44726
Kuhnehee <sheva0902@naver.com>
2019-11-28 01:18:27 +0900

    Home.vue: show movie list

--- movie-front/src/views/Home.vue
+++ movie-front/src/views/Home.vue
@@ -1,17 +1,53 @@
 <template>
   <v-col class="home">
-    <h1>Movie List</h1>
+    <!-- <h1>Movie List</h1> -->
+    <MovieList :movies="movies"/>
   </v-col>
 </template>
 
 <script>
 // @ is an alias to /src
-// import MovieList from '@/components/MovieList.vue'
+import MovieList from '@/components/MovieList.vue'
 // import router from '@/router'
-// import axios from 'axios'
+import axios from 'axios'
 
 
 export default {
   name: 'home',
+
+  data () {
+    return {
+      movies: [],
+    }
+  },
+
+  components: {
+    MovieList,
+  },
+
+  methods: {
+    getMovies() {
+      const token = sessionStorage.getItem('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+
+      axios.get('http://localhost:8000/api/v1/movie/', options)
+      .then(res => {
+        this.movies = res.data
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+
+    }, // end of getMovies()
+  }, // end of methods
+
+  mounted() {
+    this.getMovies()    
+  }
+
 }
 </script>
